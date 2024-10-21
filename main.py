8e316d54249d927b1889dd5c6711c15e3868ef7b
FranceRafaelSalacut <120196270+FranceRafaelSalacut@users.noreply.github.com>
2024-10-21 10:11:07 +0800

    Welcome Dialog now Opens a Video and  Closes on Video End

--- main.py
+++ main.py
@@ -3,39 +3,36 @@ from PyQt5.uic import loadUi
 from PyQt5.QtCore import QTimer
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 from PyQt5.QtMultimediaWidgets import QVideoWidget
+from PyQt5.QtCore import QUrl
 import sys
 import os
 
 class MainUI(QMainWindow):
     def __init__(self):
         super(MainUI, self).__init__()
-        loadUi("124-Brainrot_Language.ui")
+        loadUi("124-Brainrot-Language.ui", self)
 
-class WelcomeDialog(QDialog):
+class WelcomeDialog(QMainWindow):
     def __init__(self):
-        super().__init__()
-        loadUi('124-Brainrot_Language-Welcome.ui', self)  
+        super(WelcomeDialog, self).__init__()
+        
+        self.resize(500, 500)
+        video_widget = QVideoWidget()       
+        self.setCentralWidget(video_widget)
+        self.player = QMediaPlayer(self, QMediaPlayer.VideoSurface)
+        self.player.setVideoOutput(video_widget)
+        self.player.stop()
+        path = "pebertdey.wmv"
+        self.player.setMedia(QMediaContent(QUrl.fromLocalFile(path)))
+        self.player.play()
+        self.player.mediaStatusChanged.connect(self.on_media_status_changed)
 
-        # Retrieve the QFrame by its object name
-        self.video_frame = self.findChild(QFrame, 'videoframe')  # Use the actual object name
-
-        # Create a QVideoWidget and set it as the child of the video frame
-        self.video_widget = QVideoWidget(self.video_frame)
-        layout = QVBoxLayout(self.video_frame)
-        layout.addWidget(self.video_widget)
-
-        # Create a media player
-        self.media_player = QMediaPlayer(self)
-        video_path = "/Users/christinebagazin/Downloads/124-folder/welcome-box-vid.mp4"  # Update this with your video path
-        self.media_player.setMedia(QMediaContent(video_path))
-        self.media_player.setVideoOutput(self.video_widget)
-        self.media_player.play()
-
-        # Set a timer to close the dialog automatically after 30 seconds
-        QTimer.singleShot(30000, self.accept)
+    def on_media_status_changed(self, status):
+        if status == QMediaPlayer.EndOfMedia:
+            self.close()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MainUI()
+    window = WelcomeDialog()
     window.show()
     sys.exit(app.exec_())
